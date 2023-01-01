5334ab989fe6edbe90d88b942c58551ae558b299
hyewon11 <39722108+hyewon11@users.noreply.github.com>
2023-01-01 14:00:32 +0900

    ADD: 220101 좋다

---
+++ 2301_01/좋다.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1253
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+
+cnt = 0
+A.sort()
+for idx in range(N):
+    left, right = 0, N - 2
+    B = A[:idx] + A[idx + 1:]
+    while left < right:
+        if B[left] + B[right] < A[idx]:
+            left += 1
+        elif B[left] + B[right] > A[idx]:
+            right -= 1
+        else:
+            cnt += 1
+            break
+print(cnt)
